a3c5f008117fc5e319e90ce206714f6624a3985a
Chinmay <chinmaym49@gmail.com>
2023-07-26 20:35:56 +0530

    time

--- 21intervaltimeout.js
+++ 21intervaltimeout.js
@@ -1,13 +1,12 @@
 var x=1;
 
-var counter=setInterval(()=>{                   // async
+var counter=setInterval(()=>{                   // async (runs every 0.2s)
     if(x<=20) {
         console.log(x);
         x++;
     } else {
         clearInterval(counter);
     }
-},100);
+},200);
 
-setTimeout(()=>(console.log("done")),3000);     // async
-
+setTimeout(()=>(console.log("done")),3000);     // async (printed after 3s)
