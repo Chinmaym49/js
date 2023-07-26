c4545a6f36f6e74a73e9fc324b4df7f35995699d
Chinmay <chinmaym49@gmail.com>
2023-07-26 20:54:41 +0530

    find by class, id

--- 22selectorevent/selectorevent.js
+++ 22selectorevent/selectorevent.js
@@ -1,16 +1,26 @@
-var getColor=(e)=> {
-    return window.getComputedStyle(e).backgroundColor;
+var getColor = (e) => {
+    var c = window.getComputedStyle(e).backgroundColor;
+    c = c.replace("rgb(", "");
+    c = c.replace(")", "");
+    var lis = c.split(",");
+    for (let i = 0; i < 3; i++) {
+        lis[i] = Number(lis[i]);
+        lis[i] += 100;
+    }
+    console.log(lis);
+    c = `rgb(${lis[0]}, ${lis[1]}, ${lis[2]})`;
+    return c;
 }
 
-var red=document.querySelector(".red");
-var green=document.querySelector(".green");
-var blue=document.querySelector(".blue");
-var circle=document.querySelector(".circle");
+var red = document.querySelector("#red");
+var green = document.querySelector("#green");
+var blue = document.querySelector("#blue");
+var circle = document.querySelector("#circle");
 
-red.addEventListener("mouseenter",()=>{
-    circle.style.backgroundColor=getColor(red);
+red.addEventListener("mouseenter", () => {
+    circle.style.backgroundColor = getColor(red);
 });
 
-green.addEventListener("mouseenter",()=>(circle.style.backgroundColor=getColor(green)));
+green.addEventListener("mouseenter", () => (circle.style.backgroundColor = getColor(green)));
 
-blue.addEventListener("mouseenter",()=>(circle.style.backgroundColor=getColor(blue)));
+blue.addEventListener("mouseenter", () => (circle.style.backgroundColor = getColor(blue)));
